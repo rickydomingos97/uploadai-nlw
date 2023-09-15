652082803ff1abe8d51015b6cfc406666e556569
rickydomingos <ricardodomingos97@gmail.com>
2023-09-15 01:13:03 -0300

    chatGPT included and working ontranscriptions

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from 'fastify'
+import { fastifyCors} from '@fastify/cors'
 import { getAllPromptRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { generationAICompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(getAllPromptRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generationAICompletionRoute)
 
 app.listen({
   port: 3333,
